39189f69576037d296dda389dd7d7ce30d67258d
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-06-01 14:13:58 +0530

    Prepare for release 1.0.5

---
+++ tests/DomainGeneratorTest.php
@@ -0,0 +1,39 @@
+<?php 
+// tests/DomainGeneratorTest.php
+
+use Illuminate\Support\Facades\Artisan;
+use Orchestra\Testbench\TestCase;
+
+class DomainGeneratorTest extends TestCase
+{
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Define your environment setup here.
+    }
+
+    /**
+     * Test the make:domain command.
+     *
+     * @return void
+     */
+    public function testMakeDomainCommand()
+    {
+        $serviceName = 'TestService';
+
+        // Run the make:domain command
+        Artisan::call('make:domain', ['name' => $serviceName]);
+
+        // Check that the files were created
+        $this->assertTrue(file_exists(base_path("domain/Facades/{$serviceName}Facade.php")));
+        $this->assertTrue(file_exists(base_path("domain/Services/{$serviceName}Service.php")));
+    }
+}
+
+
+?>
